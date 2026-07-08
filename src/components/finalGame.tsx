1a17cd4563ed9679be47466a09a0614742a61c71
Juan Ruiz <juanruizcampa@gmail.com>
2026-07-08 05:53:00 +0200

    agregar referencia al diálogo en FinalGame y mostrarlo al finalizar el juego

--- src/components/finalGame.tsx
+++ src/components/finalGame.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 interface Props {
   win: boolean
   gameOver: boolean
@@ -5,14 +7,26 @@ interface Props {
 }
 
 export default function FinalGame ({ win, gameOver, reset }: Props) {
+  const dialogRef = useRef<HTMLDialogElement>(null)
   const text = win ? 'WIN' : gameOver ? 'GAME OVER' : ''
 
+  useEffect(() => {
+    const dialog = dialogRef.current
+    if (dialog == null) return
+
+    if (gameOver || win) {
+      dialog.showModal()
+    }
+
+    return () => {
+      dialog.close()
+    }
+  }, [gameOver, win])
+
   return (
-    (gameOver || win) && (
-    <dialog className="gameOver">
+    <dialog ref={dialogRef} className="gameOver">
       <h2>{text}</h2>
       <button onClick={reset}>Reset</button>
     </dialog>
-    )
   )
 }
